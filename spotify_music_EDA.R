bb4a4cc90fecbb7d1456266e9e2ae62aebf05aa5
Dina <dinaabedkh@gmail.com>
2022-06-22 17:59:20 +0300

    duration/genre

--- spotify_music_EDA.R
+++ spotify_music_EDA.R
@@ -13,61 +13,60 @@ colnames(spotify)
 head(spotify, 4)
 summary(spotify)
 View(spotify)
-# save the track_album_release_date as a Date
-spotify$track_album_release_date <- as.Date(spotify$track_album_release_date)
+spotify_songs <- spotify %>% 
+  mutate(
+    track_album_release_date = as.Date(track_album_release_date),
+    year = as.numeric(format(track_album_release_date,'%Y')),
+    era = case_when(year <= 2000 ~ "under 00s era",
+                    year > 2000 & year <= 2010 ~ "2010 era",
+                    year > 2010 ~ "modern era"),
+    duration_min = duration_ms/60000) %>%
+  select(-year, -track_album_id, -playlist_id, -duration_ms)
 
-#extract the year from the date
-spotify$year <- as.numeric(format(spotify$track_album_release_date, "%Y"))
+#duration
 
-spotify$duration_min <- spotify$duration_ms/60000
-View(spotify)
+##########
+spotify_songs %>%
+  na.omit(era) %>%
+  ggplot() +
+  geom_density(aes(duration_min, fill=playlist_genre)) +
+  facet_grid(era~playlist_genre) +
+  theme_minimal() +
+  labs(x='duration_min') +
+  theme(legend.position = 'none')
 
-ggplot(spotify, aes(x = duration_min, y = track_popularity)) + 
-  geom_jitter(shape = 1) + 
-  facet_grid(playlist_genre ~ .)
-
-
-############################################
-data1960s <- spotify %>%
-  filter(year < 1970 & year > 1959)
-ggplot(data1960s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
-  labs(title = "Relationship between Song Duration and Popularity from 1960s", y = "track_popularity", x = "Duration (min)") +
-  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
-############################################
-data1970s <- spotify %>%
-  filter(year < 1980 & year > 1969)
-ggplot(data1970s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
-  labs(title = "Relationship between Song Duration and Popularity from 1970s", y = "track_popularity", x = "Duration (min)") +
-  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
-############################################
-data1980s <- spotify %>%
-  filter(year < 1990 & year > 1979)
-ggplot(data1980s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
-  labs(title = "Relationship between Song Duration and Popularity from 1980s", y = "track_popularity", x = "Duration (min)") +
-  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
-############################################
-data1990s <- spotify %>%
-  filter(year < 2000 & year > 1989)
-ggplot(data1990s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
-  labs(title = "Relationship between Song Duration and Popularity from 1990s", y = "track_popularity", x = "Duration (min)") +
-  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
-################################################
-data2000s <- spotify %>%
-  filter(year < 2010 & year > 1999)
-ggplot(data2000s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
-  labs(title = "Relationship between Song Duration and Popularity from 2000s", y = "track_popularity", x = "Duration (min)") +
-  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
-############################################
-data2010s <- spotify %>%
-  filter(year < 2020 & year > 2010)
-ggplot(data2010s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
-  labs(title = "Relationship between Song Duration and Popularity from 2010s", y = "track_popularity", x = "Duration (min)") +
-  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
+#duration popularity throughout time
+ggplot(spotify_songs, aes(x=duration_min, y=track_popularity, col=era))+ 
+  geom_jitter( size = 1.5, alpha(0.25) )+ facet_grid(era ~ .)
 
 
-decadedata <- rbind(data1960s, data1970s, data1980s, data1990s, data2000s, data2010s)
-ggplot(decadedata, aes(x=duration_min, y=track_popularity, col=year))+ 
-  geom_jitter( size = 1.5 )+ facet_wrap(year ~ .)
+
+
+
+
+
+#loudness
+
+#boxplot
+
+ggplot(data = spotify, mapping = aes(x = playlist_genre, y=loudness, fill=playlist_genre, alpha=0.05)) +
+  geom_boxplot()+
+  theme_bw()+
+  theme(legend.position = "none")+
+  labs(x = "Playlist Genres",
+       y = "loudness",
+       title = "Distribution of loudness by using boxpplot")
+
+
+
+
+ggplot(spotify_songs, aes(x=loudness, y=track_popularity, col=playlist_genre))+ 
+  geom_jitter( size = 1.5 )+ facet_wrap(playlist_genre ~ .)
+
+
+
+
+
 
 
 
