01dd61291a1ba4ab159d2b2e865709e60fbb5b8c
Mo-Salamah <53323195+Mo-Salamah@users.noreply.github.com>
2022-06-22 19:21:22 +0300

    add more visualizations

--- spotify_music_EDA.R
+++ spotify_music_EDA.R
@@ -2,9 +2,12 @@
 library(tidyverse)
 library(skimr)
 library(corrplot)
+library(scales)
+library(ggrepel)
+library(gtools)
 
 
-songs <- read_csv("data/spotify_songs.csv")
+songs <- read_csv("data/songs_songs.csv")
 
 # view general statistics on the data set
 skim(songs)
@@ -24,11 +27,14 @@ songs_exclusive <- songs %>%
 songs_amatures <- songs %>% 
   filter(track_popularity < 5)
 
-ggplot(songs, aes(x = track_popularity, color = playlist_genre)) + 
+songs_middle <- songs %>% 
+  filter(track_popularity > 5 & track_popularity < 70)
+
+ggplot(songs, aes(x = track_popularity, y = ..density.., color = playlist_genre)) + 
   geom_freqpoly()
 
 # which genre is most popular?
-ggplot(songs, aes(x = track_popularity, color = playlist_genre)) + 
+ggplot(songs, aes(x = track_popularity, ..density.., color = playlist_genre)) + 
   geom_freqpoly() +
   coord_cartesian(ylim = c(0, 500)) + 
   coord_cartesian(xlim = c(20, 120)) + 
@@ -53,12 +59,49 @@ songs %>%
   geom_histogram()
 
 
-# what genres are most popular amongst amature creators? --------
+# what genres are most popular amongst amature/popular creators? --------
 
   
 ggplot(songs_amatures, aes(x = track_popularity, fill = playlist_genre)) +
   geom_bar(position = "fill") +
   scale_y_continuous("Proportion")
+
+ggplot(songs_popular, aes(x = track_popularity, fill = playlist_genre)) +
+  geom_histogram(binwidth = 5, position = "fill") +
+  scale_y_continuous("Proportion")
+
+ggplot(songs_middle, aes(x = track_popularity, fill = playlist_genre)) +
+  geom_histogram(binwidth = 5, position = "fill") +
+  scale_y_continuous("Proportion")
+
+# best plot so far
+ggplot(songs, aes(x = track_popularity, fill = playlist_genre)) +
+  geom_histogram(binwidth = 5, position = "fill") +
+  scale_y_continuous("Proportion")
+
+songs %>% 
+  group_by(quartile_popularity, playlist_genre) %>% 
+  summarise(proportion = percent((n()/nrow(songs))))
+
+# best table so far
+table(songs$playlist_genre, songs$quartile_popularity) %>% 
+  as.data.frame() -> tentile_popularity
+
+ggplot(tentile_popularity, aes(Var2, Freq, color = Var1, group = Var1)) +
+  geom_point() +
+  # geom_col(position = "fill") +
+  # scale_y_continuous("Proportion") +
+  # geom_line() +
+  # geom_step() +
+  # geom_path() +
+  NULL
+
+
+
+
+
+
+
 
 songs_amatures %>% 
   select(track_popularity, playlist_genre) %>% 
@@ -101,6 +144,8 @@ songs %>%
   mutate(avg_popularity = mean(track_popularity)) %>% 
   mutate(stddev_popularity = sd(track_popularity)) %>% 
   mutate(num_performed_tracks = n_distinct(track_id)) %>% 
+  ungroup() %>% 
+  mutate(quartile_popularity = quantcut(track_popularity, q = 10)) %>% 
   ungroup() -> songs
 
   
@@ -158,7 +203,9 @@ songs %>%
   
 
 
-
+ggplot(songs, aes(x = duration_min, y = track_popularity)) + 
+  geom_jitter(shape = 1) + 
+  facet_grid(playlist_genre ~ .)
 
 
 songs %>% 
@@ -178,12 +225,12 @@ ggplot(aes(decade, avg_number_genres)) +
 
 
 
-
-songs %>% 
+songs_exclusive$year <- as.numeric(songs_exclusive$year) 
+songs_exclusive %>% 
   select(- c(track_id, track_name, track_artist, track_album_id,
          track_album_name, track_album_name,
          track_album_release_date, playlist_name, playlist_id,
-         playlist_genre, playlist_subgenre, decade, year, stddev_popularity)) %>% 
+         playlist_genre, playlist_subgenre, decade)) %>% 
   cor() -> corr_matrix
 
 corr_matrix %>% 
@@ -214,9 +261,49 @@ corr_matrix[1,] %>%
 
 
 
+songs$duration_min <- songs$duration_ms/60000
+
+############################################
+data1960s <- songs %>%
+  filter(year < 1970 & year > 1959)
+ggplot(data1960s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
+  labs(title = "Relationship between Song Duration and Popularity from 1960s", y = "track_popularity", x = "Duration (min)") +
+  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
+############################################
+data1970s <- songs %>%
+  filter(year < 1980 & year > 1969)
+ggplot(data1970s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
+  labs(title = "Relationship between Song Duration and Popularity from 1970s", y = "track_popularity", x = "Duration (min)") +
+  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
+############################################
+data1980s <- songs %>%
+  filter(year < 1990 & year > 1979)
+ggplot(data1980s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
+  labs(title = "Relationship between Song Duration and Popularity from 1980s", y = "track_popularity", x = "Duration (min)") +
+  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
+############################################
+data1990s <- songs %>%
+  filter(year < 2000 & year > 1989)
+ggplot(data1990s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
+  labs(title = "Relationship between Song Duration and Popularity from 1990s", y = "track_popularity", x = "Duration (min)") +
+  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
+################################################
+data2000s <- songs %>%
+  filter(year < 2010 & year > 1999)
+ggplot(data2000s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
+  labs(title = "Relationship between Song Duration and Popularity from 2000s", y = "track_popularity", x = "Duration (min)") +
+  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
+############################################
+data2010s <- songs %>%
+  filter(year < 2020 & year > 2010)
+ggplot(data2010s, aes(x = duration_min, y = track_popularity)) + geom_jitter() +
+  labs(title = "Relationship between Song Duration and Popularity from 2010s", y = "track_popularity", x = "Duration (min)") +
+  theme_classic() + theme(plot.title = element_text(hjust = 0.5))
 
 
-
+decadedata <- rbind(data1960s, data1970s, data1980s, data1990s, data2000s, data2010s)
+ggplot(decadedata, aes(x=duration_min, y=track_popularity, col=year))+ 
+  geom_jitter( size = 1.5 )+ facet_wrap(year ~ .)
 
 
 
